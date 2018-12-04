afaeb5c57bea8a8e985f7e27884cde300df3e91e
zhousunchun <zhousunchun@gmail.com>
2018-12-05 00:26:57 +0800

    add selection sort method; add function to parse option; change the code of swap function

--- main.cpp
+++ main.cpp
@@ -1,18 +1,50 @@
+
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <vector>
 #include <algorithm>
+#include <getopt.h>
+
 
 using namespace std;
 
+void printer(const std::vector<int> & arr);
+
+static struct option long_options[] =
+{
+    {"method",required_argument, NULL, 'm'}
+};
+
 void swap(std::vector<int> &arr, const int& i, const int& j)
 {
-    arr[i] = arr[i] ^ arr[j];
-    arr[j] = arr[i] ^ arr[j];
-    arr[i] = arr[i] ^ arr[j];
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void selectionSort(std::vector<int> & arr)
+{
+    std::cout << "The result of selectionSort method"<< std::endl;
+    if(arr.empty()||arr.size()<2)
+    {
+        return ;
+    }
+
+    for(int i=0; i<arr.size()-1; i++)
+    {
+        int minIndex = i;
+        for(int j=i+1; j<arr.size(); ++j)
+        {
+            minIndex = arr[j] < arr[minIndex]?j:minIndex;
+        }
+        swap(arr,i, minIndex);
+    }
 }
 
 void bubbleSort(std::vector<int>&  arr)
 {
+    std::cout << "The result of bubbleSort method"<< std::endl;
     if(arr.empty() || arr.size()<2)
         return;
 
@@ -54,15 +86,17 @@ bool isEqual(const std::vector<int> vec1, const std::vector<int> vec2)
     return true;
 }
 
-int main()
+int main(int argc,char *argv[])
 {
     std::vector<int> vec;
     srand(time(0));
+    std::cout << "Generate a random array" << std::endl;
     for(int index = 0; index < 20; ++index)
     {
         vec.emplace_back(rand()%100);
     }
     printer(vec);
+
 
     std::vector<int> vec1, vec2;
     vec1.resize(vec.size());
@@ -70,11 +104,50 @@ int main()
     vec2.resize(vec.size());
     vec2.assign(vec.begin(),vec.end());
 
+    std::cout << "Utilize system provided method to sort the array"<< std::endl;
     system_sort(vec1);
     printer(vec1);
 
-    bubbleSort(vec2);
-    printer(vec2);
+    int option=getopt_long(argc,argv,"m:",long_options, 0);
+    if(-1 == option)
+    {
+        std::cerr << "parser parameter error!"<< std::endl;
+    }
+
+    switch(option)
+    {
+    case 'm':
+    {
+        stringstream arg;
+        std::string m_arg;
+        arg<<optarg;
+        arg>>m_arg;
+
+        if(m_arg=="bubbleSort")
+        {
+            bubbleSort(vec2);
+            printer(vec2);
+        }
+        else if(m_arg=="selectionSort")
+        {
+            selectionSort(vec2);
+            printer(vec2);
+        }
+        else
+        {
+            std::cerr<<"This program does not contain this sort method" << std::endl;
+            return -1;
+        }
+    }
+        break;
+    default:
+        {
+            std::cerr<<"This program can't parse this option" << std::endl;
+            return -1;
+        }
+        break;
+    }
+
 
     std::cout << (isEqual(vec1,vec2) ? "NICE" : "FUCKED!") << std::endl;
 
