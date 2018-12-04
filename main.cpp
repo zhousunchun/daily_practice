c532710125f05900da5ac367c02f34a6a74ee607
zhousunchun <zhousunchun@gmail.com>
2018-12-05 00:53:57 +0800

    add heapSort method

--- main.cpp
+++ main.cpp
@@ -23,6 +23,52 @@ void swap(std::vector<int> &arr, const int& i, const int& j)
     arr[j] = temp;
 }
 
+void heapInsert(std::vector<int> & arr, int index)
+{
+    while(arr[index]>arr[(index-1)/2])
+    {
+        swap(arr,index,(index-1)/2);
+        index =(index-1)/2;
+    }
+}
+
+void heapify(std::vector<int> & arr, int index, int size)
+{
+    int left = index*2 +1;
+    while(left<size)
+    {
+        int largest = left+1<size && arr[left+1] > arr[left] ? left+1:left;
+        largest = arr[largest] > arr[index] ? largest:index;
+        if(largest == index)
+        {
+            break;
+        }
+
+        swap(arr,largest,index);
+        index = largest;
+        left = index*2+1;
+    }
+}
+
+void heapSort(std::vector<int> &arr)
+{
+    if(arr.empty() || arr.size()<2)
+        return ;
+    for(int i=0; i<arr.size(); i++)
+    {
+        heapInsert(arr, i);
+    }
+
+    int size = arr.size();
+    swap(arr,0,--size);
+    while(size>0)
+    {
+        heapify(arr,0,size);
+        swap(arr,0,--size);
+    }
+}
+
+
 void selectionSort(std::vector<int> & arr)
 {
     std::cout << "The result of selectionSort method"<< std::endl;
@@ -131,6 +177,11 @@ int main(int argc,char *argv[])
         else if(m_arg=="selectionSort")
         {
             selectionSort(vec2);
+            printer(vec2);
+        }
+        else if(m_arg =="heapSort")
+        {
+            heapSort(vec2);
             printer(vec2);
         }
         else
